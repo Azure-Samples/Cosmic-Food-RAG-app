5e4a6688df9ac4359fad6b974a9c7513dd4a5d3d
John Aziz <johnaziz269@gmail.com>
2026-04-24 02:04:05 +0300

    fix: add --active flag to uv run command in entrypoint script to use active venv

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -4,4 +4,4 @@ set -e
 python3 -m pip install --upgrade pip
 python3 -m pip install uv
 uv sync --active
-uv run hypercorn 'quartapp.app:create_app()' --bind=0.0.0.0
+uv run --active hypercorn 'quartapp.app:create_app()' --bind=0.0.0.0
